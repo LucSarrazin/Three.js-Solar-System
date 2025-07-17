20806c621966f87204b774840377c697503129b2
Luc Sarrazin <luc.sarrazin@free.fr>
2025-07-17 20:38:10 +0200

    Add GLTFLoader and implement model loading for scene

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -7,11 +8,23 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+const loader = new GLTFLoader();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
+
+
+loader.load( 'public/kawai/scene.gltf', function ( gltf ) {
+
+  scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+  console.error( error );
+
+} );
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
